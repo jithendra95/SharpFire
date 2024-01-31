638c7b170df5005f72dbf03a19ee83088400e593
Jithendra Thenuwara <jithendra.thenuwara@red-gate.com>
2024-01-31 18:00:00 +0000

    Add all required methods

--- SharpFire/Database/RealtimeDatabase.cs
+++ SharpFire/Database/RealtimeDatabase.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using System.Net;
+using Newtonsoft.Json;
+using static System.GC;
 using Console = System.Console;
 
 namespace SharpFire.Database;
@@ -21,29 +23,46 @@ public class RealtimeDatabase : IDisposable
         var response = await _client
             .GetAsync(
                 $"{DatabaseUrl}/{path}.json?auth={AccessToken}");
-        var responseData = await Get(path);
+        var responseData = await response.Content.ReadAsStringAsync();
         return JsonConvert.DeserializeObject<T>(responseData);
     }
-    
-    public async Task<string> Get(string path)
+
+    public async Task<bool> Post<T>(string path, T value)
     {
         var response = await _client
-            .GetAsync(
-                $"{DatabaseUrl}/{path}.json?auth={AccessToken}");
+            .PostAsync(
+                $"{DatabaseUrl}/{path}.json?auth={AccessToken}", new StringContent(JsonConvert.SerializeObject(value)));
+       return response.StatusCode == HttpStatusCode.OK;
+    }
+    
+    public async Task<string> Put<T>(string path, T value)
+    {
+        var response = await _client
+            .PutAsync(
+                $"{DatabaseUrl}/{path}.json?auth={AccessToken}", new StringContent(JsonConvert.SerializeObject(value)));
         var responseData = await response.Content.ReadAsStringAsync();
         return responseData;
     }
     
-    public async Task<string> GetAll()
+    public async Task<bool> Patch<T>(string path, T value)
     {
         var response = await _client
-            .GetAsync(
-                $"{DatabaseUrl}/.json?auth={AccessToken}");
-        return await response.Content.ReadAsStringAsync();
+            .PatchAsync(
+                $"{DatabaseUrl}/{path}.json?auth={AccessToken}", new StringContent(JsonConvert.SerializeObject(value)));
+        return response.StatusCode == HttpStatusCode.OK;
+    }
+    
+    public async Task<bool> Delete(string path)
+    {
+        var response = await _client
+            .DeleteAsync(
+                $"{DatabaseUrl}/{path}.json?auth={AccessToken}");
+        return response.StatusCode == HttpStatusCode.OK;
     }
 
     public void Dispose()
     {
+        SuppressFinalize(this);
         _client.Dispose();
     }
 }
