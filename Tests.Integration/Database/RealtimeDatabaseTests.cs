2c0a21a4a613175bacf27f12a278074a656c6933
Jithendra Thenuwara <jithendra.thenuwara@red-gate.com>
2024-02-10 19:26:50 +0000

    improve integration tests for realtime db

--- Tests.Integration/Database/RealtimeDatabaseTests.cs
+++ Tests.Integration/Database/RealtimeDatabaseTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using AutoFixture;
+using FluentAssertions;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using SharpFire.Firebase;
@@ -10,8 +11,11 @@ public class RealtimeDatabaseTests : FirebaseTestBase
 {
     private const string TestEndpoint = "integration/test/";
     private string _testId;
+    private readonly Fixture _fixture = new();
 
     private record MockObject(string Name, int Age);
+
+    private record MockAgeObject(int Age);
 
     [OneTimeSetUp]
     public async Task OneTimeSetUp()
@@ -28,14 +32,14 @@ public class RealtimeDatabaseTests : FirebaseTestBase
     [Test]
     public async Task Get_ShouldReturnJsonString()
     {
-        var result = await FirebaseApp.RealtimeDatabase.Get($"{TestEndpoint}{_testId}");
-        
+        var result = await GetData(_testId);
+
         result.Should().BeOfType<string>();
         var mockObject = JsonConvert.DeserializeObject<MockObject>(result);
         mockObject?.Name.Should().Be("John");
         mockObject?.Age.Should().Be(25);
     }
-    
+
     [Test]
     public async Task GetWithType_ShouldReturnObject()
     {
@@ -43,12 +47,125 @@ public class RealtimeDatabaseTests : FirebaseTestBase
         result?.Name.Should().Be("John");
         result?.Age.Should().Be(25);
     }
-    
+
     [Test]
-    public async Task PosyWithType_ShouldReturnStringName()
+    public async Task GetWithInvalidEndpoint_ShouldReturnNull()
     {
-        var newObject = new MockObject("Nathan ", 30);
-        var result = await FirebaseApp.RealtimeDatabase.Post<MockObject>($"{TestEndpoint}{_testId}", newObject);
+        var result = await FirebaseApp.RealtimeDatabase.Get<MockObject>("ggg");
+        result.Should().BeNull();
+    }
+
+    [Test]
+    public async Task Post_ShouldReturnStringNameAndPersistData()
+    {
+        var mockObject = CreateMock();
+        var mockJsonString = JsonConvert.SerializeObject(mockObject);
+        var result = await FirebaseApp.RealtimeDatabase.Post($"{TestEndpoint}", mockJsonString);
+
         result.Should().NotBeNull();
+        ValidatePersistedData(result, "Nathan", 30);
+    }
+
+
+    [Test]
+    public async Task PostWithType_ShouldReturnStringNameAndPersistData()
+    {
+        var mockObject = CreateMock();
+        var result = await FirebaseApp.RealtimeDatabase.Post($"{TestEndpoint}", mockObject);
+
+        result.Should().NotBeNull();
+        ValidatePersistedData(result, mockObject.Name, mockObject.Age);
+    }
+
+    [Test]
+    public async Task Put_ShouldReturnStringAndReplaceData()
+    {
+        var testId = _fixture.Create<string>();
+        var mockObject = CreateMock();
+        var mockJsonString = JsonConvert.SerializeObject(mockObject);
+        
+        var result = await FirebaseApp.RealtimeDatabase.Put($"{TestEndpoint}{testId}", mockJsonString);
+       
+        result.Should().NotBeNull();
+
+        var savedObject = JsonConvert.DeserializeObject<MockObject>(result);
+        savedObject?.Name.Should().Be(mockObject.Name);
+        savedObject?.Age.Should().Be(mockObject.Age);
+
+        ValidatePersistedData(testId, mockObject.Name, mockObject.Age);
+    }
+
+    [Test]
+    public async Task PutWithType_ShouldReturnObjectAndReplaceData()
+    {
+        var testId = _fixture.Create<string>();
+        var mockObject = CreateMock();
+        
+        var result = await FirebaseApp.RealtimeDatabase.Put($"{TestEndpoint}{testId}", mockObject);
+        
+        result.Should().NotBeNull();
+        result?.Name.Should().Be(mockObject.Name);
+        result?.Age.Should().Be(mockObject.Age);
+
+        ValidatePersistedData(testId, mockObject.Name, mockObject.Age);
+    }
+
+    [Test]
+    public async Task Patch_ShouldReturnPatchedDataStringAndUpdateData()
+    {
+        var mockObject = CreateMock();
+        
+        var id = await FirebaseApp.RealtimeDatabase.Post($"{TestEndpoint}", mockObject);
+        
+        var putAge= _fixture.Create<int>();
+        var newObject = new MockAgeObject(putAge);
+        
+        var result = await FirebaseApp.RealtimeDatabase.Patch($"{TestEndpoint}{id}", JsonConvert.SerializeObject(newObject));
+        
+        result.Should().NotBeNull();
+        
+        var savedObject = JsonConvert.DeserializeObject<MockAgeObject>(result);
+        savedObject?.Age.Should().Be(newObject.Age);
+
+        ValidatePersistedData(_testId, mockObject.Name, newObject.Age);
+    }
+
+    [Test]
+    public async Task PatchWithType_ShouldReturnPatchedDataAndUpdateData()
+    {
+        var mockObject = CreateMock();
+        var id = await FirebaseApp.RealtimeDatabase.Post($"{TestEndpoint}", mockObject);
+        
+        var putAge= _fixture.Create<int>();
+        var newObject = new MockAgeObject(putAge);
+        var result = await FirebaseApp.RealtimeDatabase.Patch($"{TestEndpoint}{id}", newObject);
+        
+        result.Should().NotBeNull();
+        result?.Age.Should().Be(newObject.Age);
+
+        ValidatePersistedData(_testId, mockObject.Name, newObject.Age);
+    }
+
+
+    private static Task<string> GetData(string testId)
+    {
+        return FirebaseApp.RealtimeDatabase.Get($"{TestEndpoint}{testId}");
+    }
+
+    private static async void ValidatePersistedData(string testId, string expectedName, int expectedAge)
+    {
+        var result = await GetData(testId);
+        var mockObject = JsonConvert.DeserializeObject<MockObject>(result);
+        mockObject?.Name.Should().Be(expectedName);
+        mockObject?.Age.Should().Be(expectedAge);
+    }
+    
+    
+    private MockObject CreateMock()
+    {
+        var name = _fixture.Create<string>();
+        var age = _fixture.Create<int>();
+
+        return new MockObject(name, age);
     }
 }
