3b7d748f7e7aea187026ee26e50920a433167343
Jithendra Thenuwara <jithendra.thenuwara@red-gate.com>
2024-02-14 19:31:10 +0000

    remove unused exception constructor

--- SharpFire/Database/Exceptions/NoDataFoundException.cs
+++ SharpFire/Database/Exceptions/NoDataFoundException.cs
@@ -2,17 +2,10 @@
 
 public class NoDataFoundException : Exception
 {
-    public NoDataFoundException()
-    {
-    }
 
     public NoDataFoundException(string message)
         : base(message)
     {
     }
 
-    public NoDataFoundException(string message, Exception inner)
-        : base(message, inner)
-    {
-    }
 }
