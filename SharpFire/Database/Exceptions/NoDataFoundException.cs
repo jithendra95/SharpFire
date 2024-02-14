13949f5777162f5400899491975c6e1a073c269c
Jithendra Thenuwara <jithendra.thenuwara@red-gate.com>
2024-02-14 19:26:15 +0000

    Add custom exception

---
+++ SharpFire/Database/Exceptions/NoDataFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpFire.Database.Exceptions;
+
+public class NoDataFoundException : Exception
+{
+    public NoDataFoundException()
+    {
+    }
+
+    public NoDataFoundException(string message)
+        : base(message)
+    {
+    }
+
+    public NoDataFoundException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
