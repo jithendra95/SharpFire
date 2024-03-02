960f4a01f86a2fde2ea89dc8dcb6070af3867dd4
Jithendra Thenuwara <jithendra.thenuwara@red-gate.com>
2024-03-02 20:13:12 +0000

    introduce tests for path to secret

--- Tests.Unit/Firebase/FirebaseAppTests.cs
+++ Tests.Unit/Firebase/FirebaseAppTests.cs
@@ -8,7 +8,7 @@ public class FirebaseAppTests
 {
     private const string MockAccessToken = "hhhggg-hgifbghdf=4378683";
     private const string MockDatabaseUrl = "https://ssd-fft.europe-west1.firebasedatabase.app";
-
+   
     [TearDown]
     public void TearDown()
     {
@@ -17,20 +17,30 @@ public class FirebaseAppTests
 
 
     [Test]
-    public void Create_ShouldInstantiate_RealtimeDatabase()
+    public void Create_ShouldInstantiate_RealtimeDatabaseWithAccessToken()
     {
         var appOptions =
-            new AppOptions( MockAccessToken, MockDatabaseUrl);
+            new AppOptions(MockAccessToken, MockDatabaseUrl);
         FirebaseApp.Create(appOptions);
         FirebaseApp.RealtimeDatabase.Should().NotBeNull();
     }
 
-    [TestCase("", MockDatabaseUrl)]
-    [TestCase(MockAccessToken, "")]
-    public void Create_ShouldFail_WhenAccessTokenOrDatabaseURLIsEmpty(string path, string databaseUrl)
+    [Test]
+    public void Create_ShouldFail_WhenDatabaseUrlIsEmpty()
+
     {
         var appOptions =
-            new AppOptions( path, databaseUrl);
+            new AppOptions { DatabaseUrl = String.Empty, AccessToken = MockAccessToken, PathToSecretFile = "path/to/secret" };
+        var action = () => FirebaseApp.Create(appOptions);
+        action.Should().Throw<Exception>();
+    }
+
+    [Test]
+    public void Create_ShouldFail_WhenAccessTokenAndPathToSecretIsEmpty()
+
+    {
+        var appOptions =
+            new AppOptions { DatabaseUrl = MockDatabaseUrl, AccessToken = string.Empty, PathToSecretFile = string.Empty };
         var action = () => FirebaseApp.Create(appOptions);
         action.Should().Throw<Exception>();
     }
@@ -39,13 +49,13 @@ public class FirebaseAppTests
     public void Create_ShouldFail_WhenInvokedMultipleTimes_WithoutDisposing()
     {
         var appOptions =
-            new AppOptions( MockAccessToken, MockDatabaseUrl);
+            new AppOptions(MockAccessToken, MockDatabaseUrl);
         FirebaseApp.Create(appOptions);
 
         var action = () => FirebaseApp.Create(appOptions);
         action.Should().Throw<Exception>().WithMessage("FirebaseApp is already initialized");
     }
-    
+
     [Test]
     public void RealtimeDatabase_ShouldShouldThrowException_WhenNotInitialized()
     {
